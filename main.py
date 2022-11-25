381dbcf763b174606d2a781e188c30c925a67b44
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2022-11-25 09:09:10 +0300

    update

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from Computer import *
 
 laptop = Computer(128)
-#instaling appplications
+
 laptop.install_application("VS Code", 16)
+laptop.install_application("Word", 32)
 laptop.install_application("Google Chrome", 64)
 
-#running appplications
+laptop.run_application("Word")
 laptop.run_application("VS Code")
-laptop.run_application("Google Chrome")
+laptop.run_application("Word")
 
-laptop.delete_application("Dota")
+laptop.delete_application("Word")
+laptop.run_application("Google Chrome")
 
 laptop.display_applications_information()
 laptop.display_all_information()
+print(laptop.get_log_information())
+print(laptop.information)
+
 laptop.optimize_memory()
 
-#Informaion about applications
-print(laptop.log)
-
-#Informaion about segments
+laptop.display_applications_information()
+laptop.display_all_information()
+print(laptop.get_log_information())
 print(laptop.information)
